0b7ddf87915ca35ef948fab1e383fd28b7260d7e
Markus Koller <markus@snafu.ch>
2026-04-28 01:35:14 +0200

    chore: use a background `ColorRect` instead of setting clear color

--- main.gd
+++ main.gd
@@ -1,5 +1,4 @@
 extends Node
 
 func _ready() -> void:
-  RenderingServer.set_default_clear_color(Color.BLACK)
-  get_tree().change_scene_to_file.call_deferred("res://snake/snake.tscn")
+  get_tree().change_scene_to_file.call_deferred('res://snake/snake.tscn')
