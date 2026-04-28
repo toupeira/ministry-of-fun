c652f1be6b0dfba69e378b04804760bb61a9ac29
Markus Koller <markus@snafu.ch>
2026-04-28 21:48:22 +0200

    feat(main): add initial game browser

--- game_info.gd
+++ game_info.gd
@@ -3,13 +3,17 @@ class_name GameInfo
 static var current: String
 
 static var games: Dictionary[String, Dictionary] = {
+  main = {
+    name = "Ministry of Fun",
+    hidden = true,
+  },
   snake = {
     name = "Snake",
     genre = "Action",
     description = "Eat apples to grow your snake and avoid obstacles!",
-    history = ' '.join([
-      'Inspired by the arcade game "Blockade" released by Gremlin Industries in 1976,',
-      '"Snake" became widely popular when it was launched on Nokia mobile phones in 1997.'
+    history = '\n'.join([
+      '"Snake" became widely popular when it was launched on Nokia mobile phones in 1997.',
+      'It was based on the arcade game "Blockade", released by Gremlin Industries in 1976.',
     ]),
   },
 }
--- main.gd
+++ main.gd
@@ -1,4 +1,37 @@
 extends Node
 
+@onready var game_list: ItemList = %GameList
+@onready var game_info: RichTextLabel = %GameInfo
+
 func _ready() -> void:
   get_tree().change_scene_to_file.call_deferred('res://snake/snake.tscn')
+  return
+
+  for game_id in GameInfo.games:
+    var game := GameInfo.games[game_id]
+    if game.get('hidden', false):
+      continue
+
+    var game_name: String = game.name
+    var index := game_list.add_item(game_name)
+    game_list.set_item_metadata(index, game_id)
+    game_list.set_item_tooltip_enabled(index, false)
+
+  game_list.grab_focus()
+  game_list.select(0)
+
+func _input(event: InputEvent) -> void:
+  if event is InputEventMouse:
+    Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _on_game_list_item_selected(index: int) -> void:
+  var game_id: String = game_list.get_item_metadata(index)
+  var game := GameInfo.games[game_id]
+  game_info.text = "[font_size=12][color=#aaa]Description:[/color][/font_size]\n%s\n\n[font_size=12][color=#aaa]History:[/color][/font_size]\n%s" % [
+    game.get('description', ''),
+    game.get('history', '')
+  ]
+
+func _on_game_list_item_activated(index: int) -> void:
+  var game_id: String = game_list.get_item_metadata(index)
+  get_tree().change_scene_to_file('res://%s/%s.tscn' % [game_id, game_id])
